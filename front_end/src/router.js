f67a8708d7f1814a7e80c0db4c9a011e991b57ff
xingzhe5213 <8614468+xingzhe5213@user.noreply.gitee.com>
2022-02-24 16:15:15 +0800

    新增perInfo模块路由

--- front_end/src/router.js
+++ front_end/src/router.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './pages/Login.vue'
 import Home from './pages/Home.vue'
+import perInfo from './pages/perInfo.vue'
 
 Vue.use(Router)
 
 export default new Router({
+    mode: 'history',
     routes: [
         {
             path: '/',
@@ -22,6 +24,12 @@ export default new Router({
                 roles: ['admin', 'user']
             },
             children: [
+                {
+                    path: '/perInfo',
+                    name: '个人中心',
+                    component: perInfo,
+                    hidden: true
+                }
             ]
         },
     ]
