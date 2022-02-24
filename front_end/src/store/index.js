7ccc12ace28adcc7fc6eb77c27c89d8998cf7e17
xingzhe5213 <8614468+xingzhe5213@user.noreply.gitee.com>
2022-02-24 16:12:37 +0800

    修改store本地存储

--- front_end/src/store/index.js
+++ front_end/src/store/index.js
@@ -6,14 +6,7 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
         routes: [],
-        sessions: {},
-        token:null,
-        hrs: [],
-        currentSession: null,
         currentHr: JSON.parse(window.sessionStorage.getItem("user")),
-        filterKey: '',
-        stomp: null,
-        isDot: {}
     },
     mutations: {
         initCurrentUser(state, hr) {
@@ -21,22 +14,8 @@ const store = new Vuex.Store({
         },
         initRoutes(state, data) {
             state.routes = data;
-        },
-        initToken(state, token) {
-            state.token = token;
         }
-    },
-    actions: {
-
     }
-})
-
-store.watch(function (state) {
-    return state.sessions
-}, function (val) {
-    localStorage.setItem('vue-chat-session', JSON.stringify(val));
-}, {
-    deep: true
 })
 
 export default store;
