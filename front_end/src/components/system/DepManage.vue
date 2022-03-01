027ade12f249620a27fe45ecf8bb4369b8d2d4ff
xingzhe5213 <8614468+xingzhe5213@user.noreply.gitee.com>
2022-03-01 20:52:27 +0800

    新增系统管理-部门管理模块

---
+++ front_end/src/components/system/DepManage.vue
@@ -0,0 +1,203 @@
+<template>
+  <div style="width: 500px;">
+    <el-input
+        placeholder="请输入部门名称进行搜索..."
+        prefix-icon="el-icon-search"
+        v-model="filterText">
+    </el-input>
+    <el-tree
+        :data="deps"
+        :props="defaultProps"
+        :expand-on-click-node="false"
+        :filter-node-method="filterNode"
+        ref="tree">
+            <span class="custom-tree-node" style="display: flex;justify-content: space-between;width: 100%;"
+                  slot-scope="{ node, data }">
+        <span>{{data.name }}</span>
+        <span>
+          <el-button
+              type="primary"
+              size="mini"
+              class="depBtn"
+              @click="() => showAddDepView(data)">
+            添加部门
+          </el-button>
+          <el-button
+              type="danger"
+              size="mini"
+              class="depBtn"
+              @click="() => deleteDep(data)">
+            删除部门
+          </el-button>
+        </span>
+      </span>
+    </el-tree>
+    <el-dialog
+        title="添加部门"
+        :visible.sync="dialogVisible"
+        width="30%">
+      <div>
+        <table>
+          <tr>
+            <td>
+              <el-tag>上级部门</el-tag>
+            </td>
+            <td>{{pname}}</td>
+          </tr>
+          <tr>
+            <td>
+              <el-tag>部门名称</el-tag>
+            </td>
+            <td>
+              <el-input v-model="dep.name" placeholder="请输入部门名称..."></el-input>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="doAddDep">确 定</el-button>
+  </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DepManage",
+  data() {
+    return {
+      dialogVisible: false,
+      filterText: '',
+      dep: {
+        name: '',
+        parentid: -1
+      },
+      pname: '',
+      deps: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  watch: {
+    filterText(val) {
+      this.$refs.tree.filter(val);
+    }
+  },
+  mounted() {
+    this.initDeps();
+  },
+  methods: {
+    initDep() {
+      this.dep = {
+        name: '',
+        parentid: -1
+      }
+      this.pname = '';
+    },
+    addDep2Deps(deps, dep) {
+      for (let i = 0; i < deps.length; i++) {
+        let d = deps[i];
+        if (d.id == dep.parentid) {
+          d.children = d.children.concat(dep);
+          if (d.children.length > 0) {
+            d.parent = true;
+          }
+          return;
+        } else {
+          this.addDep2Deps(d.children, dep);
+        }
+      }
+    },
+    doAddDep() {
+      console.log(this.dep);
+      this.Post("/api/sys/base/department", this.dep).then(res => {
+        if (res.code==200) {
+          this.addDep2Deps(this.deps, res.data[0]);
+          this.dialogVisible = false;
+          this.$message({
+            message: '添加成功！',
+            type: 'success'
+          });
+          //初始化变量
+          this.initDep();
+        }else{
+          this.$message({
+            message: '添加失败，请检查部门名称是否重复！',
+            type: 'error'
+          });
+        }
+      })
+    },
+    removeDepFromDeps(p,deps, id) {
+      for(let i=0;i<deps.length;i++){
+        let d = deps[i];
+        if (d.id == id) {
+          deps.splice(i, 1);
+          if (deps.length == 0) {
+            p.parent = false;
+          }
+          return;
+        }else{
+          this.removeDepFromDeps(d,d.children, id);
+        }
+      }
+    },
+    deleteDep(data) {
+      if (data.children.length>0) {
+        this.$message.error("请先删除所有子部门！！");
+      } else {
+        this.$confirm('此操作将永久删除【' + data.name + '】部门, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).then(() => {
+          this.Delete("/api/sys/base/department/?id="+data.id).then(res=>{
+            if (res.code==200) {
+              this.removeDepFromDeps(null,this.deps,data.id);
+              this.$message({
+                type: 'success',
+                message: '删除成功！'
+              });
+            }else if(res.code==201){
+              this.$message({
+                type: 'info',
+                message: '该部门人数不为0，删除失败！'
+              });
+            }
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: '已取消删除'
+          });
+        });
+      }
+    },
+    showAddDepView(data) {
+      this.pname = data.name;
+      this.dep.parentid = data.id;
+      this.dialogVisible = true;
+    },
+    initDeps() {
+      this.Get("/api/sys/base/department").then(res => {
+        if (res) {
+          this.deps = res.data;
+        }
+      })
+    },
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.name.indexOf(value) !== -1;
+    }
+  }
+}
+</script>
+
+<style>
+.depBtn {
+  padding: 2px;
+}
+</style>
