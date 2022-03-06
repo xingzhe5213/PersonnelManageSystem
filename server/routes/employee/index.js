89efbacb038777df794a8fb95dabbb6ff26339df
xingzhe5213 <8614468+xingzhe5213@user.noreply.gitee.com>
2022-03-07 00:40:27 +0800

    新增奖惩模块路由

--- server/routes/employee/index.js
+++ server/routes/employee/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 // 员工信息
 const employee = require('./employee');
+// 员工奖惩
+const reward = require('./reward');
 
 router.use('/', employee);
+router.use('/reward/', reward);
 
 module.exports = router;
