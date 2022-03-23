7933f9709f5dcbbf9ca22692c57ee1678c8310f7
xingzhe5213 <8614468+xingzhe5213@user.noreply.gitee.com>
2022-03-23 17:36:19 +0800

    格式化代码

--- server/mysql/config.js
+++ server/mysql/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    database: "permanage",  //数据库名
-    user:"xingzhe",         //数据库用户名
-    password:"xingzhe",     //数据库密码
-    host: "1.116.234.220",
-    port : 3306,            //端口号
+	database: "hris",  //数据库名
+	user: "root",         //数据库用户名
+	password: "123456",     //数据库密码
+	host: "127.0.0.1",
+	port: 3306,            //端口号
 };
